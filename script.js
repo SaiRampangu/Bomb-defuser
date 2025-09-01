0be3689b000331bd57265d5c76f498e87092ae4e
Saidulu Rampangu <sairam.rampangu1528@gmail.com>
2025-09-02 01:47:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let userInputEl = document.getElementById("userInput");
+let timerEl = document.getElementById("timer");
+
+let countDown = timerEl.textContent;//formate mm:ss
+let minutes = parseInt(countDown.slice(0, 2));
+let seconds = parseInt(countDown.slice(3, 5));
+
+let intervalId = setInterval(function(){
+    
+    if (minutes === 0 && seconds === 0){
+        clearInterval(intervalId);
+        timerEl.textContent = "BOOM! the bomb exploded";
+        return;
+    }
+    if (seconds === 0){
+        if (minutes > 0){
+            minutes -= 1;
+            seconds = 59;
+        }
+    }else{
+        seconds -= 1;
+    }
+    timerEl.textContent = minutes.toString().padStart(2,0) + ":" + seconds.toString().padStart(2,0);
+
+
+},50);
+
+userInputEl.addEventListener("keydown", function(event){
+    if (event.key === "Enter" && userInputEl.value === "defuse" && minutes > 0){
+        clearInterval(intervalId);
+        timerEl.textContent = "Bomb defused successfully";
+    }
+})
